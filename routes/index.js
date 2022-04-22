b63a8165a4670a75652061f0b39eaf54dd20f284
Ragdabakr <ragdabakr@yahoo.com>
2022-04-22 10:25:17 +0200

    products

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const categoryRoute = require('./categoryRoute');
 const subCategoryRoute = require('./subCategoryRoute');
 const brandRoute = require('./brandRoute');
-// const productRoute = require('./productRoute');
+const productRoute = require('./productRoute');
 const userRoute = require('./userRoute');
 const authRoute = require('./authRoute');
 // const reviewRoute = require('./reviewRoute');
@@ -15,7 +15,7 @@ const mountRoutes = (app) => {
   app.use('/api/v1/categories', categoryRoute);
   app.use('/api/v1/subcategories', subCategoryRoute);
   app.use('/api/v1/brands', brandRoute);
-//   app.use('/api/v1/products', productRoute);
+  app.use('/api/v1/products', productRoute);
   app.use('/api/v1/users', userRoute);
   app.use('/api/v1/auth', authRoute);
 //   app.use('/api/v1/reviews', reviewRoute);

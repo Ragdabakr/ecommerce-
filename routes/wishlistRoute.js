17e349f3457ef8797ab016392322238a0fa16bdf
Ragdabakr <ragdabakr@yahoo.com>
2022-04-25 08:57:29 +0200

    wishlist

--- routes/wishlistRoute.js
+++ routes/wishlistRoute.js
@@ -10,7 +10,7 @@ const {
 
 const router = express.Router();
 
-router.use(authService.protect, authService.allowedTo('user' ,'admin'));
+router.use(authService.protect, authService.allowedTo('user'));
 
 router.route('/').post(addProductToWishlist).get(getLoggedUserWishlist);
 

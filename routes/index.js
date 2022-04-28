0e79a5751a12b8bfead3bff1b97e0818c98eab01
Ragdabakr <ragdabakr@yahoo.com>
2022-04-28 02:22:12 +0200

    order

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const wishlistRoute = require('./wishlistRoute');
 const addressRoute = require('./addressRoute');
 const couponRoute = require('./couponRoute');
 const cartRoute = require('./cartRoute');
-// const orderRoute = require('./orderRoute');
+const orderRoute = require('./orderRoute');
 
 const mountRoutes = (app) => {
   app.use('/api/v1/categories', categoryRoute);
@@ -23,7 +23,7 @@ const mountRoutes = (app) => {
   app.use('/api/v1/addresses', addressRoute);
   app.use('/api/v1/coupons', couponRoute);
   app.use('/api/v1/cart', cartRoute);
-//   app.use('/api/v1/orders', orderRoute);
+  app.use('/api/v1/orders', orderRoute);
 };
 
 module.exports = mountRoutes;
